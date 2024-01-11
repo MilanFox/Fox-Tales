68c48357a08cb66643d504f977eaec1003012648
Milan Fox <milan.fox@diva-e.com>
2024-01-11 17:00:04 +0100

    "Add Group" Screen

---
+++ lib/screens/add_group_screen.dart
@@ -0,0 +1,117 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fox_tales/data/colors.dart';
+import 'package:fox_tales/providers/users_provider.dart';
+
+class AddGroupScreen extends ConsumerStatefulWidget {
+  const AddGroupScreen({super.key});
+
+  @override
+  ConsumerState<AddGroupScreen> createState() {
+    return _AddGroupScreenState();
+  }
+}
+
+class _AddGroupScreenState extends ConsumerState<AddGroupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _members = [FirebaseAuth.instance.currentUser!.uid];
+
+  @override
+  Widget build(BuildContext context) {
+    final users = ref.watch(usersProvider);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Add new Group'),
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        actions: [
+          GestureDetector(
+              child: Container(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Badge(
+                    label: Text(_members.length.toString()),
+                    backgroundColor: primary,
+                    child: const Icon(
+                      Icons.check_box,
+                      size: 30,
+                    ),
+                  )))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Group Name'),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Please add a group name";
+                  }
+                  return null;
+                },
+                onSaved: (value) {},
+              ),
+              const SizedBox(height: 40),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (ctx, index) {
+                    return GestureDetector(
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.symmetric(vertical: 4),
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.grey),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              users[index].name,
+                              style: TextStyle(
+                                fontWeight: _members.contains(users[index].uid)
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                                color: _members.contains(users[index].uid)
+                                    ? primary
+                                    : Colors.black,
+                                fontSize: 14,
+                              ),
+                            ),
+                            const Spacer(),
+                            Icon(
+                              Icons.group_add,
+                              color: _members.contains(users[index].uid)
+                                  ? primary
+                                  : Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          if (_members.contains(users[index].uid)) {
+                            _members.remove(users[index].uid);
+                          } else {
+                            _members.add(users[index].uid);
+                          }
+                        });
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fox_tales/data/colors.dart';
+import 'package:fox_tales/screens/add_group_screen.dart';
 
 class MessagesScreen extends ConsumerStatefulWidget {
   const MessagesScreen({super.key});
@@ -13,8 +15,28 @@ class MessagesScreen extends ConsumerStatefulWidget {
 class _MessagesScreenState extends ConsumerState<MessagesScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("You have not been added to any groups yet."),
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 16.0,
+          right: 16.0,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+                return const AddGroupScreen();
+              }));
+            },
+            child: const Icon(
+              Icons.add_box,
+              size: 50,
+              color: primary,
+            ),
+          ),
+        ),
+        const Center(
+          child: Text("You have not been added to any groups yet."),
+        ),
+      ],
     );
   }
 }
