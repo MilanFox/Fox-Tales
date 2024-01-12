6f50e8b905ddae7a90a3fb704355f835e7236501
Milan Fox <milan.fox@diva-e.com>
2024-01-12 13:37:37 +0100

    Group Filter

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fox_tales/data/colors.dart';
@@ -37,17 +38,23 @@ class _MessagesScreenState extends ConsumerState<MessagesScreen> {
 
               final groups =
                   snapshot.data!.docs.firstWhere((doc) => doc.id == 'groups');
+
               final groupData = groups['groupData']
                   .entries
-                  .map((entry) => entry.value)
+                  .map((entry) => entry.value as Map<String, dynamic>)
+                  .toList();
+
+              final currentUser = FirebaseAuth.instance.currentUser!.uid;
+              final filteredGroups = List<Map<String, dynamic>>.from(groupData)
+                  .where((element) => element['members'].contains(currentUser))
                   .toList();
 
               return ListView.builder(
-                  itemCount: groupData.length,
+                  itemCount: filteredGroups.length,
                   itemBuilder: (ctx, index) {
                     return ChatGroupTeaser(
-                      name: groupData[index]['name'],
-                      lastMessage: groupData[index]['lastMessage'],
+                      name: filteredGroups[index]['name'],
+                      lastMessage: filteredGroups[index]['lastMessage'],
                     );
                   });
             }),
